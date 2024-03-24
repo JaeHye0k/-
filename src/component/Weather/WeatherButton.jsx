f6ff921c04501f334e3bedfeaad92bb9398a174c
JaeHye0k <ahhpc2012@naver.com>
2024-03-24 23:44:35 +0900

    [Feat] 날씨에 따라 배경 달라지는 기능 추가, 선택한 버튼 표시하는 기능 추가

--- src/component/Weather/WeatherButton.jsx
+++ src/component/Weather/WeatherButton.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import style from "../../styles/Weather.module.css";
 
-const WeatherButton = ({ cities, setCity, btnX }) => {
+const WeatherButton = ({ cities, setCity, btnX, selectedCity }) => {
   return (
     <div
-      className={style.weather_buttons}
+      id={style.weather_buttons}
       style={{ transform: `translateX(${btnX}vw)` }}
     >
-      {cities.map((item, idx) => (
-        <Button variant="warning" onClick={() => setCity(item)} key={idx}>
-          {item === "" ? "Current Location" : item}
+      {cities.map((city, idx) => (
+        <Button
+          className={`${style.weather_button} ${
+            city === selectedCity && style.selected_button
+          }`}
+          variant="warning"
+          onClick={() => setCity(city)}
+          key={idx}
+        >
+          {city === "" ? "Current Location" : city}
         </Button>
       ))}
     </div>
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { useEffect, useState, useRef } from "react";
 import WeatherBox from "../component/Weather/WeatherBox";
 import WeatherButton from "../component/Weather/WeatherButton";
-import style from "../styles/Weather.module.css";
 import IndexButton from "../component/IndexButton";
+import { Spinner } from "react-bootstrap";
+import style from "../styles/Weather.module.css";
 
 // 1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
 // 2. 날씨 정보에는 도시 이름, 섭씨 온도, 화씨 온도, 날씨 상태
@@ -11,15 +12,22 @@ import IndexButton from "../component/IndexButton";
 // 4. 버튼을 누를 때마다 해당 위치의 날씨 정보가 화면에 출력된다.
 // 5. 현재 위치 버튼을 누르면 다시 현재 위치 기반의 날씨가 나온다. (얘만 좀 다름)
 // 6. 날씨 정보 데이터를 가져오는 동안 로딩 스피너가 돈다.
+const backgroundWeather = [
+  "broken_clouds",
+  "clear_sky",
+  "overcast_clouds",
+  "rain",
+  "mist",
+  "snow",
+];
 
 const Weather = () => {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
   const [btnX, setBtnX] = useState(20);
-  // const [curCarousel, setCurCarousel] = useState(1);
-  // const [carouselTransition, setCarouselTransition] = useState(
-  //   "transform 500ms ease-in-out"
-  // );
+  const [loading, setLoading] = useState(true);
+  const [apiError, setAPIError] = useState("");
+  const [weatherDescription, setWeatherDescription] = useState(null);
 
   const cities = [
     "",
@@ -29,9 +37,12 @@ const Weather = () => {
     "Sapporo",
     "Los Angeles",
     "Las Vegas",
+    "Paris",
+    "New York",
+    "Cebu",
   ];
-  const apiKey = "37145ca12efe07b866fade0a0ab89107";
-
+  const API_KEY = "37145ca12efe07b866fade0a0ab89107";
+  // 현재 위치 구하기
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
@@ -39,32 +50,51 @@ const Weather = () => {
       getWeatherByCurrentLocation(lat, lon);
     });
   };
+  // 현재 위치 날씨 구하기
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`;
-    const response = await fetch(url);
-    const data = await response.json();
-    setWeather(data);
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&&units=metric`;
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setWeather(data);
+      formatDescription(data.weather[0].description);
+    } catch (err) {
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
+  // 도시 날씨 구하기
+  const getWeatherByCityName = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&&units=metric`;
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setWeather(data);
+      formatDescription(data.weather[0].description);
+    } catch (err) {
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
+  const formatDescription = (description) => {
+    let formatted = description.split(" ");
+    let fullName = formatted.join("_");
+    let partOfName = formatted.at(-1);
+    // 완전히 일치하는 날씨 배경 이미지가 있다면 해당 날씨 배경으로 변경
+    if (backgroundWeather.includes(fullName)) {
+      setWeatherDescription(fullName);
+    }
+    // 완전히 일치하는 게 없으면 비슷한 날씨로라도 변경.
+    else {
+      partOfName = backgroundWeather.find((e) => e.includes(partOfName));
+      setWeatherDescription(partOfName);
+    }
   };
 
-  const getWeatherByCityName = async (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
-    const response = await fetch(url);
-    const data = await response.json();
-    setWeather(data);
-  };
-  // const makeNewCarouselItem = (arr) => {
-  //   const itemStart = arr[0];
-  //   const itemEnd = arr[arr.length - 1];
-  //   const newCarousel = [itemEnd, ...arr, itemStart];
-  //   return newCarousel;
-  // };
-  // const MyComponent = () => {
-  //   const ref = useRef(null);
-  //   useEffect(() => {
-  //     console.log(ref.current);
-  //   }, [ref.current]);
-  //   return <div ref={ref}>Hello</div>;
-  // };
   const clickNextButton = () => {
     setBtnX(btnX - 20);
   };
@@ -75,16 +105,28 @@ const Weather = () => {
   useEffect(() => {
     if (city === "") getCurrentLocation();
     else getWeatherByCityName(city);
-    console.log(city);
   }, [city]);
 
   return (
     <div className={style.weather_container}>
       <IndexButton />
+      <video
+        src={
+          process.env.PUBLIC_URL + `/assets/videos/${weatherDescription}.mov`
+        }
+        className={style.video_background}
+        autoPlay={true}
+        loop={true}
+        muted={true}
+      ></video>
       <div className={style.weather_screen}>
         <div className={style.weather_contents}>
           <div className={style.weather_box}>
-            <WeatherBox weather={weather} />
+            {loading ? (
+              <Spinner animation="border" className={style.bs_spinner} />
+            ) : (
+              <WeatherBox weather={weather} />
+            )}
           </div>
           <div className={style.carousel_button}>
             <div className={style.weather_button_container}>
@@ -97,7 +139,12 @@ const Weather = () => {
                 </button>
               )}
 
-              <WeatherButton cities={cities} setCity={setCity} btnX={btnX} />
+              <WeatherButton
+                cities={cities}
+                setCity={setCity}
+                btnX={btnX}
+                selectedCity={city}
+              />
               <button
                 className={style.next_button}
                 onClick={() => clickNextButton()}
