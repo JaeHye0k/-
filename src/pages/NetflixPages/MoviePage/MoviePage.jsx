2ff440aaf693ad7e2ea451f03d98362605c2a804
JaeHye0k <ahhpc2012@naver.com>
2024-04-08 00:27:06 +0900

    [Refactor] refactor change banner image

--- src/pages/NetflixPages/MoviePage/MoviePage.jsx
+++ src/pages/NetflixPages/MoviePage/MoviePage.jsx
@@ -1,20 +1,6 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { intervalActions } from "../../../redux/Netflix/reducers/intervalSlice";
 
 const MoviePage = () => {
-  const isOnBannerInterval = useSelector(
-    (state) => state.interval.isOnBannerInterval
-  );
-  const bannerIntervalId = useSelector(
-    (state) => state.interval.bannerIntervalId
-  );
-  const dispatch = useDispatch();
-  // 배너 이미지 전환 interval 종료
-  if (isOnBannerInterval) {
-    clearInterval(bannerIntervalId);
-    dispatch(intervalActions.setIsOnBannerInterval(false));
-  }
   return <div>MoviePage</div>;
 };
 
