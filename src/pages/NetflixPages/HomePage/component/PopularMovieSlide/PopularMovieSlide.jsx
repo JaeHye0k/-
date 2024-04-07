fb6dd1686260be9abfd2120cd37f027364cb51bc
JaeHye0k <ahhpc2012@naver.com>
2024-04-08 00:26:06 +0900

    [Feat] Add popular movie slide feature

--- src/pages/NetflixPages/HomePage/component/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/NetflixPages/HomePage/component/PopularMovieSlide/PopularMovieSlide.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../../hooks/netflix/usePopularMovies";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import MovieCard from "../MovieCard/MovieCard";
+import "./PopularMovieSlide.style.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 6,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+  },
+};
 
 const PopularMovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -11,7 +30,21 @@ const PopularMovieSlide = () => {
     return <h1>{error.message}</h1>;
   }
 
-  return <div>PopularMovieSlide</div>;
+  return (
+    <div className="popular-movie-slide">
+      <h3>Popular Movies</h3>
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        itemClass="movie-slider p-1"
+        containerClass="carousel-container"
+      >
+        {data.results.map((movie, key) => (
+          <MovieCard movie={movie} key={key} />
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default PopularMovieSlide;
