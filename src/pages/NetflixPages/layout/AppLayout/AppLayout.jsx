1b666f4f253cb5f496f5ae611493684df3a1d141
JaeHye0k <ahhpc2012@naver.com>
2024-04-10 01:27:11 +0900

    [Feat] add search movie

--- src/pages/NetflixPages/layout/AppLayout/AppLayout.jsx
+++ src/pages/NetflixPages/layout/AppLayout/AppLayout.jsx
@@ -11,9 +11,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./AppLayout.style.css";
 import Intro from "../../pages/Intro/Intro";
+import { useNavigate } from "react-router-dom";
 
 const AppLayout = () => {
   const [isIntro, setIsIntro] = useState(true);
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  const searchByKeyword = (e) => {
+    e.preventDefault();
+    navigate(`./movies?q=${keyword}`);
+    setKeyword("");
+  };
   setTimeout(() => {
     setIsIntro(false);
   }, 4500);
@@ -31,31 +40,36 @@ const AppLayout = () => {
                   width={100}
                 />
               </Navbar.Brand>
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-              >
-                <Nav.Link as={Link} to="">
-                  Home
-                </Nav.Link>
-                <Nav.Link as={Link} to="movies">
-                  Movies
-                </Nav.Link>
-              </Nav>
-              <Form>
-                <InputGroup>
-                  <Form.Control
-                    data-bs-theme="dark"
-                    type="search"
-                    placeholder="Search"
-                    className="me-2 search-box"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-danger">
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                  </Button>
-                </InputGroup>
-              </Form>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: "100px" }}
+                >
+                  <Nav.Link as={Link} to="">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="movies">
+                    Movies
+                  </Nav.Link>
+                </Nav>
+                <Form onSubmit={searchByKeyword}>
+                  <InputGroup>
+                    <Form.Control
+                      data-bs-theme="dark"
+                      type="search"
+                      placeholder="Search"
+                      className="me-2 search-box"
+                      aria-label="Search"
+                      value={keyword}
+                      onChange={(e) => setKeyword(e.target.value)}
+                    />
+                    <Button variant="outline-danger" type="submit">
+                      <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </Button>
+                  </InputGroup>
+                </Form>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </header>
