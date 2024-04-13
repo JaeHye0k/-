6c4a932b78b6cb271935444e3b218948c17fb74b
JaeHye0k <ahhpc2012@naver.com>
2024-04-14 00:43:50 +0900

    [Add] add link to MovieDetailPage

--- src/pages/NetflixPages/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NetflixPages/pages/NotFoundPage/NotFoundPage.jsx
@@ -19,13 +19,13 @@ const NotFoundPage = () => {
   return (
     <div className="not-found-page">
       <section className="message-404 text-white">
-        <div>페이지를 찾을 수 없습니다.</div>
+        <h1>페이지를 찾을 수 없습니다.</h1>
         <div></div>
       </section>
       <section className="collage-404">
         <div className="collage-404-images">
           {collage?.map((item) => (
-            <a>
+            <a href={`/netflix/movies/${item.id}`}>
               <img
                 src={`https://media.themoviedb.org/t/p/w300${item?.backdrop_path}`}
               />
