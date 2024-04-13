fb5d19fd2837f02679d8c6c858fdf13ebcca0575
JaeHye0k <ahhpc2012@naver.com>
2024-04-14 00:50:12 +0900

    [Feat] add change language feature

--- src/pages/NetflixPages/hooks/useSearchMovies.js
+++ src/pages/NetflixPages/hooks/useSearchMovies.js
@@ -3,14 +3,16 @@ import { api } from "../utills/api";
 
 const fetchSearchMovie = (keyword, page, language) => {
   return keyword
-    ? api.get(`/search/movie?query=${keyword}&page=${page}`)
-    : api.get(`/movie/popular?page=${page}`);
+    ? api.get(
+        `/search/movie?query=${keyword}&page=${page}&language=${language}`
+      )
+    : api.get(`/movie/popular?page=${page}&language=${language}`);
 };
 
 export const useSearchMoviesQuery = (keyword, page = 1, language = "en-US") => {
   return useQuery({
     queryKey: ["movie-search", keyword, page],
-    queryFn: () => fetchSearchMovie(keyword, page),
+    queryFn: () => fetchSearchMovie(keyword, page, language),
     select: (result) => result.data,
   });
 };
